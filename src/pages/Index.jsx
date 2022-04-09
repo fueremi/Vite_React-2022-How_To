b4a39f542892345f9c55aaa10256e6b083ca506d
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-04-10 01:50:00 +0700

    major(*): initiate project

---
+++ src/pages/Index.jsx
@@ -0,0 +1,12 @@
+const Index = () => {
+  return (
+    <div className="container mx-auto min-h-screen py-8 px-4">
+      <h1 className="font-bold text-4xl text-slate-800 text-center">
+        Vite React + React Router V6 + Recoil + Tailwind CSS
+      </h1>
+
+    </div>
+  );
+};
+
+export default Index;
