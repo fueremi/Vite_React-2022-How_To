c4cc8a978d7a691cf20528a2d18ca26becadb828
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-04-10 01:50:51 +0700

    feat(section): adding installation section

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -48,6 +48,126 @@ const Index = () => {
           </p>
         </div>
       </section>
+
+      <section id="installation" className="mt-8">
+        <h2 className="font-semibold text-2xl text-slate-800 mb-2 text-center">
+          Installation
+        </h2>
+
+        <div className="mb-4">
+          <h3 className="font-medium text-xl text-slate-800">Vite React</h3>
+          <p className="text-justify font-normal text-sm text-slate-800">
+            <span className="font-semibold text-indigo-400">
+              Why using Vite?
+            </span>{" "}
+            In short, because vite so{" "}
+            <span className="font-semibold text-indigo-400">fast</span> and so{" "}
+            <span className="font-semibold text-indigo-400">light</span>. You
+            can see this{" "}
+            <a
+              href="https://dev.to/d4rkener/why-you-should-use-vite-2dh9"
+              target="_blank"
+              className="font-semibold text-emerald-500 border-b-2 border-emerald-500"
+            >
+              article
+            </a>{" "}
+            for more reason why Vite so popular now.
+          </p>
+          <div className="my-2 font-semibold text-sm text-slate-800">
+            Command :
+          </div>
+          <h4 className="font-semibold text-sm text-slate-800">NPM 6.x</h4>
+          <div className="flex justify-between items-center bg-[#F5F8FA] py-2 px-6 mb-2">
+            <code className="rounded-lg font-normal text-sm text-dark-800">
+              <span className="text-indigo-400">npm</span> create vite@latest
+              app_name --template [react, react-ts]
+            </code>
+            <span className="text-slate-400 hover:text-slate-800 hover:cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                />
+              </svg>
+            </span>
+          </div>
+
+          <h4 className="font-semibold text-sm text-slate-800">NPM 7.x</h4>
+          <div className="flex justify-between items-center bg-[#F5F8FA] py-2 px-6 mb-2">
+            <code className="rounded-lg font-normal text-sm text-dark-800">
+              <span className="text-indigo-400">npm</span> create vite@latest
+              app_name -- --template [react, react-ts]
+            </code>
+            <span className="text-slate-400 hover:text-slate-800 hover:cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                />
+              </svg>
+            </span>
+          </div>
+
+          <h4 className="font-semibold text-sm text-slate-800">Yarn</h4>
+          <div className="flex justify-between items-center bg-[#F5F8FA] py-2 px-6">
+            <code className="rounded-lg font-normal text-sm text-dark-800">
+              <span className="text-indigo-400">yarn</span> create vite
+              my-vue-app_name --template [react, react-ts]
+            </code>
+            <span className="text-slate-400 hover:text-slate-800 hover:cursor-pointer">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+                />
+              </svg>
+            </span>
+          </div>
+        </div>
+        <div className="mb-4">
+          <h3 className="font-medium text-xl text-slate-800">Tailwind CSS</h3>
+          <p className="text-justify font-normal text-sm text-slate-800">
+            <span className="font-semibold text-indigo-400">
+              Why using Tailwind CSS?
+            </span>{" "}
+            Basically because you can styling without ever leave your{" "}
+            <span className="font-semibold text-indigo-400">.jsx file.</span>
+            You can see this{" "}
+            <a
+              href="https://blog.bitsrc.io/5-reasons-to-use-tailwind-css-with-react-native-1e7999c40b2e"
+              target="_blank"
+              className="font-semibold text-emerald-500 border-b-2 border-emerald-500"
+            >
+              article
+            </a>{" "}
+            for more reason why React is good match with Tailwind CSS.
+          </p>
+        </div>
+      </section>
     </div>
   );
 };
